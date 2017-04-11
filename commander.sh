1127f3a0b26e3681e0ce5719caff1171f83515ef
Dmitry <listat88@gmail.com>
2017-04-11 18:04:11 +0300

    INNO-423 Add docker env

--- commander.sh
+++ commander.sh
@@ -223,18 +223,18 @@ local_env_menu() {
         docker-compose up -d
         echo "Waiting until servers will be ready to recieve connections"
         sleep 20
-        docker-compose run banking-php sh -c "composer install && php artisan droptables && php artisan migrate && php artisan db:seed && php artisan l5:generate"
+        docker-compose run banking-php sh -c "composer install && php artisan droptables --env=docker && php artisan migrate --env=docker && php artisan db:seed --env=docker && php artisan l5:generate"
         source .env
         echo "Generating API KEY"
         docker-compose run intvoice-php sh -c "composer install"
-        API_TOKEN="$(docker-compose run intvoice-php php artisan get_banking_key --key=foo-api-key --password=lalelu123)"
+        API_TOKEN="$(docker-compose run intvoice-php php artisan --env=docker get_banking_key --key=foo-api-key --password=lalelu123)"
         echo "Setting API KEY to: ${API_TOKEN}"
-        cp ${INTVOICE_PATH}/.env ${INTVOICE_PATH}/.env.bak
-        sed "s/^BANKING_API_TOKEN=.*/BANKING_API_TOKEN=${API_TOKEN}/g" "${INTVOICE_PATH}/.env.bak" > ${INTVOICE_PATH}/.env
-        cp ${INTVOICE_PATH}/.env.testing ${INTVOICE_PATH}/.env.testing.bak
-        sed "s/^BANKING_API_TOKEN=.*/BANKING_API_TOKEN=${API_TOKEN}/g" "${INTVOICE_PATH}/.env.testing.bak" > ${INTVOICE_PATH}/.env.testing
+        cp ${INTVOICE_PATH}/.env.docker ${INTVOICE_PATH}/.env.bak
+        sed "s/^BANKING_API_TOKEN=.*/BANKING_API_TOKEN=${API_TOKEN}/g" "${INTVOICE_PATH}/.env.bak" > ${INTVOICE_PATH}/.env.docker
+        cp ${INTVOICE_PATH}/.env.docker.testing ${INTVOICE_PATH}/.env.testing.bak
+        sed "s/^BANKING_API_TOKEN=.*/BANKING_API_TOKEN=${API_TOKEN}/g" "${INTVOICE_PATH}/.env.testing.bak" > ${INTVOICE_PATH}/.env.docker.testing
         echo "Start working with INTVOICE"
-        docker-compose run intvoice-php sh -c "php artisan droptables && php artisan migrate && php artisan db:seed && php artisan l5:generate"
+        docker-compose run intvoice-php sh -c "php artisan droptables --env=docker && php artisan migrate --env=docker && php artisan db:seed --env=docker && php artisan l5:generate --env=docker"
         read -p "Press enter to continue"
         main_menu
         ;;
--- docker_intvoice_acceptance_test_runner.sh
+++ docker_intvoice_acceptance_test_runner.sh
@@ -9,12 +9,13 @@ echo "Waiting for data servers will able to recieve connections"
 sleep 30
 
 echo "Refill banking test database"
-docker-compose -f docker-compose.test.yml run test-banking-php sh -c "php artisan droptables --env=testing && php artisan migrate --env=testing && php artisan db:seed --env=testing"
+docker-compose -f docker-compose.test.yml run test-banking-php sh -c "php artisan droptables --env=docker.testing && php artisan migrate --env=docker.testing && php artisan db:seed --env=docker.testing"
 docker-compose -f docker-compose.test.yml stop
 
 echo "Refill intvoice test database"
-docker-compose -f docker-compose.test.yml run test-intvoice-php sh -c "php artisan droptables --env=testing && php artisan migrate --env=testing && php artisan db:seed --env=testing"
+
+docker-compose -f docker-compose.test.yml run test-intvoice-php sh -c "php artisan droptables --env=docker.testing && php artisan migrate --env=docker.testing && php artisan db:seed --env=docker.testing"
 docker-compose -f docker-compose.test.yml stop
 
 echo "Running tests"
-docker-compose -f docker-compose.test.yml run test-intvoice-php php vendor/codeception/codeception/codecept run $FILE --env=testing
+docker-compose -f docker-compose.test.yml run test-intvoice-php php vendor/codeception/codeception/codecept run $FILE --env=docker.testing
