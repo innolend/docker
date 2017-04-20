1226a50352ec4aa4ba66e95702abd4c5422efe92
Dmitry <listat88@gmail.com>
2017-04-20 16:42:30 +0300

    INNO-423 Update test runner

--- docker_intvoice_acceptance_test_runner.sh
+++ docker_intvoice_acceptance_test_runner.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 FILE=$1
-echo "Turn off all test containers"
-docker-compose -f ../INTVOICE/docker-compose.test.yml stop
-docker-compose -f ../INTVOICE/docker-compose.test.yml rm -f
-docker-compose -f ../INTVOICE/docker-compose.test.yml up -d
-echo "Waiting for data servers will able to recieve connections"
-sleep 30
-
-docker-compose -f ../INTVOICE/docker-compose.test.yml stop
 
 echo "Running tests"
 docker-compose -f ../INTVOICE/docker-compose.test.yml run test-intvoice-php php vendor/codeception/codeception/codecept run $FILE --env=docker.testing
