3c1d6c4ea5ebd10a8c3184d26b3bdc788910598a
Dmitry <listat88@gmail.com>
2017-04-21 13:08:50 +0300

    INNO-423 change selenium resolution

--- docker_intvoice_acceptance_test_runner.sh
+++ docker_intvoice_acceptance_test_runner.sh
@@ -3,4 +3,8 @@
 FILE=$1
 
 echo "Running tests"
+docker-compose -f ../INTVOICE/docker-compose.test.yml stop
+docker-compose -f ../INTVOICE/docker-compose.test.yml rm
+docker-compose -f ../INTVOICE/docker-compose.test.yml up -d
+sleep 10
 docker-compose -f ../INTVOICE/docker-compose.test.yml run --rm test-intvoice-php php vendor/codeception/codeception/codecept run $FILE --env=docker.testing
