5c3bf523b34192f8c79eaf90d335fbba89d4eaf1
Dmitry <listat88@gmail.com>
2017-05-02 19:08:20 +0300

    INNO-423 Modify pre build

--- docker_env_pre_build.sh
+++ docker_env_pre_build.sh
@@ -1,10 +1,10 @@
 echo "Building composer"
-docker-compose -f ../BANKING/docker-compose.yml run banking-php sh -c "composer install"
-docker-compose -f ../INTVOICE/docker-compose.yml run intvoice-php sh -c "composer install"
+docker-compose -f ../BANKING/docker-compose.yml run --rm banking-php sh -c "composer install"
+docker-compose -f ../INTVOICE/docker-compose.yml run --rm intvoice-php sh -c "composer install"
 
 echo "Building node modules && gulp"
-docker-compose -f ../BANKING/docker-compose.yml run banking-packages sh -c "yarn"
-docker-compose -f ../INTVOICE/docker-compose.yml run intvoice-packages sh -c "yarn add node-sass && yarn add gulp-sass && yarn && gulp"
+docker-compose -f ../BANKING/docker-compose.yml run --rm banking-packages sh -c "yarn install"
+docker-compose -f ../INTVOICE/docker-compose.yml run --rm intvoice-packages sh -c "yarn install && npm link node-sass && gulp"
 
 echo "Rebuilding cache"
 docker-compose -f ../BANKING/docker-compose.yml run --rm sh -c "php artisan optimize --force && php artisan config:cache && php artisan route:cache"
