01c5b5eb4e9db5889cb6d6662fe1c8d429883d46
Dmitry <listat88@gmail.com>
2017-05-05 18:50:10 +0300

    INNO-423 Add health check

--- docker_intvoice_acceptance_test_runner.sh
+++ docker_intvoice_acceptance_test_runner.sh
@@ -3,4 +3,4 @@
 FILE=$1
 
 echo "Running tests"
-docker-compose -f ../INTVOICE/docker-compose.test.yml run --rm test-intvoice-php sh -c "sleep 10 && php vendor/codeception/codeception/codecept run $FILE --env=docker.testing"
+docker-compose -f ../INTVOICE/docker-compose.test.yml run --rm test-intvoice-php sh -c "php vendor/codeception/codeception/codecept run $FILE --env=docker.testing"
